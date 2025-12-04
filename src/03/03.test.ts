f44a07ad077461d6eb7e4133393d1e14c0358b8f
Olaren <git@olaren.dev>
2025-12-03 20:28:10 -0500

    day 3

---
+++ src/03/03.test.ts
@@ -0,0 +1,18 @@
+import { assertEquals } from "@std/assert";
+import { parse, partOne, partTwo } from "./03.ts";
+
+const input = await Deno.readTextFile(`src/03/example.txt`);
+const parsed = parse(input);
+
+Deno.test("Day 1 - part 1 - should solve example", () => {
+    const result = partOne(parsed);
+
+    assertEquals(result, 357);
+});
+
+Deno.test("Day 2 - part 2 - should solve example", () => {
+    const result = partTwo(parsed);
+
+    assertEquals(result, 3121910778619);
+});
+
---
+++ src/03/03.ts
@@ -0,0 +1,53 @@
+export function parse(input: string) {
+    return input.split("\n").filter((a) => a !== "");
+}
+
+export function partOne(input: ReturnType<typeof parse>) {
+    let res = 0;
+
+    for (const batt of input) {
+        let max = 0;
+
+        for (let i = 0; i < batt.length - 1; i++) {
+            for (let j = i + 1; j < batt.length; j++) {
+                const num = Number(`${batt[i]}${batt[j]}`);
+
+                if (num > max) {
+                    max = num;
+                }
+            }
+        }
+
+        res += max;
+    }
+
+    return res;
+}
+
+export function partTwo(input: ReturnType<typeof parse>) {
+    let res = 0;
+
+    for (const batt of input) {
+        let leftBoundary = -1;
+        let max = "";
+
+        for (let charsLeft = 12; charsLeft > 0; charsLeft--) {
+            const start = batt.length - charsLeft;
+            let maxIndex = start;
+
+            for (let i = start; i > leftBoundary; i--) {
+                if (batt[i] >= batt[maxIndex]) {
+                    maxIndex = i;
+                }
+            }
+
+            leftBoundary = maxIndex;
+            max += batt[maxIndex];
+        }
+
+        res += Number(max);
+    }
+
+    return res;
+}
+
